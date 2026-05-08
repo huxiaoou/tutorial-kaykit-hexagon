1cd7895a280a4bec804314db319df06c8c25380b
huxiaoou <516984451@qq.com>
2026-05-08 22:43:33 +0800

    Further updated tools/meshlib_exporter.gd

--- tools/meshlib_exporter.gd
+++ tools/meshlib_exporter.gd
@@ -1,34 +1,46 @@
 @tool
 extends EditorScript
 
-const SOURCE_DIR = "res://assests/gltf/tiles/base/"
+const SOURCE_DIR: String = "res://assests/gltf"
 const OUTPUT_PATH = "res://resources/meshlibs/tiles.meshlib"
+
+
+func list_files_recursive(path: String, file_list: Array[String]):
+    var dir = DirAccess.open(path)
+    if dir:
+        dir.list_dir_begin()
+        var file_name = dir.get_next()
+        while file_name != "":
+            if dir.current_is_dir():
+                var subfolder_path = path.path_join(file_name)
+                list_files_recursive(subfolder_path, file_list)
+            else:
+                file_list.append(path.path_join(file_name))
+            file_name = dir.get_next()
+    else:
+        print("An error occurred when trying to access the path: ", path)
+    return
 
 
 func _run() -> void:
     print("Begin to export gltfs to meshlib")
-    print("Source dir: ", SOURCE_DIR)
-    var mesh_lib = MeshLibrary.new()
-    process_dir(SOURCE_DIR, mesh_lib)
-    save(mesh_lib)
+    process_dir(SOURCE_DIR)
     print("Export completed.")
     return
 
 
-func process_dir(source_dir: String, mesh_lib: MeshLibrary) -> void:
-    var dir = DirAccess.open(source_dir)
-    if dir:
-        var item_id: int = 0
-        var files: PackedStringArray = dir.get_files()
-        for file: String in files:
-            if not file.ends_with(".gltf"):
-                continue
-            var path = source_dir + file
-            print("Processing: ", path)
-            process_gltf(path, mesh_lib, item_id, file)
-            item_id += 1
-    else:
-        print("Error: Failed to open directory ", source_dir)
+func process_dir(source_dir: String) -> void:
+    var mesh_lib = MeshLibrary.new()
+    var item_id: int = mesh_lib.get_item_list().size()
+    var all_files: Array[String] = []
+    list_files_recursive(source_dir, all_files)
+    for file: String in all_files:
+        if not file.ends_with(".gltf"):
+            continue
+        print("Processing %d: %s" % [item_id, file])
+        process_gltf(file, mesh_lib, item_id, file)
+        item_id += 1
+    save(mesh_lib, OUTPUT_PATH)
     return
 
 
@@ -53,9 +65,9 @@ func find_mesh_node(node: Node) -> MeshInstance3D:
     return null
 
 
-func save(mesh_lib: MeshLibrary) -> void:
-    var error = ResourceSaver.save(mesh_lib, OUTPUT_PATH)
+func save(mesh_lib: MeshLibrary, dst_path: String) -> void:
+    var error = ResourceSaver.save(mesh_lib, dst_path)
     if error == OK:
-        print("Successfully exported MeshLibrary to: ", OUTPUT_PATH)
+        print("Successfully exported MeshLibrary to: ", dst_path)
     else:
         print("Error saving MeshLibrary: ", error)
