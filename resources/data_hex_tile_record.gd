aef113b535ca0774920b1650ac31aec2100c0522
huxiaoou <516984451@qq.com>
2026-05-12 21:49:36 +0800

    Add delete logic to world_editor

--- resources/data_hex_tile_record.gd
+++ resources/data_hex_tile_record.gd
@@ -5,5 +5,6 @@ class_name DataHexTileRecord
 @export var mesh_name: String = ""
 @export var id: int = -1
 
+
 func is_equal(other: DataHexTileRecord) -> bool:
     return mesh_name == other.mesh_name and id == other.id
--- scenes/world_editor.gd
+++ scenes/world_editor.gd
@@ -9,7 +9,7 @@ class_name WorldEditor
 
 var manager_hextile: Dictionary[String, HexTile] = { }
 var selected_hextile: HexTile = null
-var records: Dictionary[Vector2i, bool] = { }
+var manager_records: Dictionary[Vector2i, DataHexTileRecord] = { }
 
 
 func _ready() -> void:
@@ -29,41 +29,78 @@ func _ready() -> void:
     return
 
 
+func match_hextile(data_record: DataHexTileRecord) -> HexTile:
+    for hextile in manager_hextile.values():
+        if hextile.data.mesh_name == data_record.mesh_name:
+            return hextile
+    return null
+
+
+func match_coords(data_record: DataHexTileRecord) -> Vector2i:
+    for coords in manager_records.keys():
+        if manager_records[coords].is_equal(data_record):
+            return coords
+    return Vector2.INF
+
+
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("place_hex_tile"):
-        if selected_hextile == null:
-            print("No hextile selected!")
-            return
-        if selected_hextile.preview_activated:
+        if selected_hextile:
             selected_hextile.deactivate_preview()
             _place_hextile()
             selected_hextile.activate_preview()
+        else:
+            print("No hextile selected!")
+    elif event.is_action_pressed("delete_hex_tile"):
+        if selected_hextile:
+            print("Cannot delete hex tile while a hextile is selected! Deselect first.")
+            return
+        _delete_hextile()
+
     elif event.is_action_pressed("debug_increase"):
-        _select_random_hexltile()
-        selected_hextile.activate_preview()
+        if selected_hextile == null:
+            _select_random_hexltile()
+            selected_hextile.activate_preview()
+        else:
+            print("Hex tile already selected: ", selected_hextile.data.mesh_name)
     elif event.is_action_pressed("debug_decrease"):
-        _deselect_and_deactivate_preview()
+        if selected_hextile:
+            selected_hextile.deactivate_preview()
+            _deselect_hextile()
+        else:
+            print("No hextile selected to deselect!")
     return
 
 
 func _place_hextile() -> void:
     var hextile_coords: Vector2i = cursor.curr_hex_tile_coords
-    if records.get(hextile_coords, false):
+    if manager_records.get(hextile_coords, null):
         print("Hex tile already exists at ", hextile_coords)
         return
 
     var hextile_point: Vector3 = cursor.curr_point
-    selected_hextile.add_instance_at(hextile_point + Vector3(0, 1, 0))
+    manager_records[hextile_coords] = selected_hextile.add_instance_at(hextile_point + Vector3(0, 1, 0))
     print("Clicked at %s, place %s hextile at %s" % [hextile_point, selected_hextile.data.mesh_name, hextile_coords])
-    records[hextile_coords] = true
+    return
+
+
+func _delete_hextile() -> void:
+    var data_record: DataHexTileRecord = manager_records.get(cursor.curr_hex_tile_coords, null)
+    if data_record:
+        selected_hextile = match_hextile(data_record)
+        if selected_hextile:
+            var last_data_record: DataHexTileRecord = selected_hextile.remove_instance_from_index(data_record.id)
+            var last_hextile_coords: Vector2i = match_coords(last_data_record)
+            manager_records[last_hextile_coords] = data_record
+            manager_records.erase(cursor.curr_hex_tile_coords)
+            selected_hextile = null
+        print("Delete hex tile at ", cursor.curr_hex_tile_coords)
+    else:
+        print("No hex tile to delete at ", cursor.curr_hex_tile_coords)
     return
 
 
 func _select_random_hexltile() -> void:
-    if selected_hextile != null:
-        print("A hextile is already selected!")
-        return
-
     var hextile_names: Array[String] = manager_hextile.keys()
     if hextile_names.is_empty():
         print("No hextiles available in manager!")
@@ -75,12 +112,7 @@ func _select_random_hexltile() -> void:
     return
 
 
-func _deselect_and_deactivate_preview() -> void:
-    if selected_hextile == null:
-        print("No hextile is currently selected!")
-        return
-    if selected_hextile.preview_activated:
-        selected_hextile.deactivate_preview()
+func _deselect_hextile() -> void:
     print("Deselected hextile: ", selected_hextile.data.mesh_name)
     selected_hextile = null
     return
