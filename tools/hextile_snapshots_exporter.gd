e61dad89257de622fe2e52cf0c828f251aa5f51f
huxiaoou <516984451@qq.com>
2026-05-16 19:40:31 +0800

    Optimize tools/hextile_snapshots_exporter.gd

--- tools/hextile_snapshots_exporter.gd
+++ tools/hextile_snapshots_exporter.gd
@@ -2,14 +2,23 @@ extends Node3D
 
 class_name HexTileSnapshotsExporter
 
-@export var meshlib: MeshLibrary
+@export var snapshot_size: Vector2i = Vector2i(512, 512)
+@export var meshlib: MeshLibrary = preload("res://resources/meshlibs/tiles.meshlib")
+@export var output_folder: String = "res://assests/snapshots/"
+@export var camera_position: Vector3 = Vector3(0.0, 2.0, 2.5)
+
+const SNAPHOT_FORMAT: String = "png"
+
+@onready var camera_3d: Camera3D = $Camera3D
 
 
 func _ready() -> void:
+    camera_3d.position = camera_position
+
     var mgr_hextile: Dictionary[String, HexTile] = Utils.setup_manager_hextile(meshlib)
     print(get_window().size)
     get_window().content_scale_mode = Window.CONTENT_SCALE_MODE_DISABLED
-    get_window().size = Vector2i(512, 512)
+    get_window().size = snapshot_size
     print(get_window().size)
     for hextile: HexTile in mgr_hextile.values():
         await get_tree().process_frame
@@ -27,7 +36,7 @@ func get_hextile_snapshot(hextile: HexTile) -> ImageTexture:
     add_child(sample_mesh)
 
     await RenderingServer.frame_post_draw
-    var img_name: String = "res://assests/snapshots/" + hextile.data.mesh_name + ".png"
+    var img_name: String = output_folder + hextile.data.mesh_name + "." + SNAPHOT_FORMAT
     get_viewport().get_texture().get_image().save_png(img_name)
     sample_mesh.queue_free()
     print(img_name)
